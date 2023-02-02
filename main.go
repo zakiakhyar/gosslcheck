9f25cd193a82b0e1470f02112559e86980fb9050
zakiakhyar <zaki.akhyar@outlook.com>
2023-02-03 05:00:51 +0700

    Initial repo - menambahkan main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	host := "kejati-aceh.kejaksaan.go.id"
+	port := 443
+
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", host, port), nil)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	cert := conn.ConnectionState().PeerCertificates[0]
+	//fmt.Println("Subject:", cert.Subject)
+	//fmt.Println("Issuer: ", cert.Issuer)
+	expiredate := cert.NotAfter.Format("02/01/2006")
+	duration := cert.NotAfter.Sub(time.Now())
+	hari := duration.Round(24*time.Hour).Hours() / 24
+	//fmt.Println(host, "Expires:", expiredate, "in", hari, "days")
+	fmt.Printf("%s,%s,%.0f\n", host, expiredate, hari)
+}
